5a790d0049ee4b3c956397975d97f65b8dec6004
Rajpateriya <pateriyaraj52@gmail.com>
2024-09-02 00:27:11 +0530

    socket io connection

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const socket = require(socket.io);
+const socket = require("socket.io");
+const exp = require('constants');
 const app = express();
 
 const server = http.createServer(app);
-const io = 
+const io = socket(server);
+
+app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+
+io.on("connection" , function(socket){
+    console.log("connected");
+})
+app.get("/",(req,res)=>{
+    res.render("index");
+})
 
 server.listen(3000, ()=>{
     console.log(`Server is listening on port 3000`)
